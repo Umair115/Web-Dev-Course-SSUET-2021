ad4a440538d458728bfb06517bf5d64d0be3604c
Umair Shehzad <41817167+Umair115@users.noreply.github.com>
2021-10-03 14:48:40 +0500

    Add files via upload

---
+++ class4.js
@@ -0,0 +1,31 @@
+// console.log("Hello World!")
+
+// for(var i=1;i<11;i++){
+//     console.log("5 x",i," = ",5*i);
+// }
+
+
+// for(var i=0;i<=20;i++){
+//     if(i%2 != 0){
+//         console.log(i,"is an Even Number")
+//     }
+// }
+
+// var _name = prompt("Enter Your Name");
+// console.log(typeof(_name));
+
+
+// => take number for which you have to print the table
+// => take number for iteration
+// => print the table
+var age = prompt("which times of table you wanna a print")
+var 
+
+it  = prompt("Please enter the limit")
+
+for(var aa= 1; aa<=limit ; aa++)
+{
+    var total=(age*aa);
+console.log(age,"x",aa,"=",total);
+}
+
