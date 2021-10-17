099c380e75feaddfb0f82b59642f8ec503e3a1d2
Umair Shehzad <41817167+Umair115@users.noreply.github.com>
2021-10-17 15:12:11 +0500

    Add files via upload

---
+++ class8.js
@@ -0,0 +1,26 @@
+var user_object = {
+    name : "Umair",
+    age : 24,
+    cnic : "42301-xxxxxxx-x",
+    phone : "03431279121",
+    email : "umair42795@gmail.com" 
+}
+
+console.log(user_object)
+
+user_object['phone'] = "03352899624"
+
+console.log(user_object)
+
+user_object['email_recovery'] = "ammarshehzad20@gmail.com";
+
+console.log(user_object)
+
+
+var user_object={}
+user_object["name"]=prompt("Enter user name:")
+user_object["phone"]=prompt("Enter user phone:")
+user_object["cnic"]=prompt("Enter user cnic:")
+console.log(user_object)
+
+
